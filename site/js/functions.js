7054b4699aa9270841104ac188886182cf9988a2
Ilkome <ilya.komichev@gmail.com>
2015-02-08 14:36:58 +0800

    upd

--- site/js/functions.js
+++ site/js/functions.js
@@ -4,7 +4,16 @@
 */
 
 $(document).ready(function() {
-	//	#slider
+
+
+	//	#preventDefault
+	// ===============================================
+	$('a[href="#"]').click(function(e) {
+		e.preventDefault();
+	});
+
+
+	/*//	#slider
 	// ===============================================
 	if ( $.isFunction($.fn.lightSlider) ) {
 		$(".js-slider-reviews").lightSlider({
@@ -25,16 +34,6 @@ $(document).ready(function() {
 			pagerWidthMin:1,
 		});
 	}
-
-
-
-	//	#banner
-	// ===============================================
-	$(".js-close-banner-text").on("click", function(e){
-		e.preventDefault();
-		$(this).closest(".js-banner-text").slideUp();
-	})
-	
 
 
 	//	#menu-drop
@@ -66,102 +65,63 @@ $(document).ready(function() {
 		}
 	});
 
-
-
-	// #popup map
+*/
+	// #search
 	// ===============================================
-	$(".js-popupmap-link").on("click", function(e) {
+	$(".js-search-form-action").on("click", function(e) {
 		e.preventDefault();
 
 		var thisis = $(this);
-		var popupId = thisis.data("mapitem-id")
-		var box = $(this).closest(".js-popupmap-box");
-		var content = box.find(".js-popupmap-content");
-		var items = content.find(".js-popupmap-item");
-		var currentItem = content.find('.js-popupmap-item[data-mapitem-id="'+popupId+'"]')
-
-		items.stop().fadeOut();
-		currentItem.stop().fadeIn();
-	});
-
-
-
-	//	#dropmenu
-	// ===============================================
-	$(".js-dropmenu").hover(
-		function() {
-			$(this).find(".js-dropmenu-link").addClass("is-open");
-			$(this).find(".js-dropmenu-content").stop().slideDown();
-		}, function() {
-			$(this).find(".js-dropmenu-link").removeClass("is-open");
-			$(this).find(".js-dropmenu-content").stop().slideUp('fast');
-		}
-	);
-
-
-
-	//	#select
-	// ===============================================
-	var selectBox = $('.select-wrap');
-	var selectBoxItems = selectBox.find('.option-list');
-	var selectBoxHeight = selectBox.outerHeight();
-	selectBox.removeClass("open");
-	
-	selectBox.hover(
-		function() {
-			$(this).find('.option-list').stop().slideDown('fast').css({top: selectBoxHeight});
-			$(this).find(".select").addClass('is-open');
-		}, function() {
-			$(this).find('.option-list').stop().fadeOut('fast');
-			$(this).find(".select").removeClass('is-open');
-		}
-	);
-
-	$('.select-wrap .option-list li').click(function() {
-		var title = $(this).closest('.select-wrap').find('.select .title');
-		var option = $(this).html();
-		$(this).closest('.select-wrap').find('.select-value').val( $(this).attr('data-value') );
-		title.empty();
-		title.html(option);
-		$(this).closest('.option-list').stop().fadeOut('fast');
-		$(this).closest('.select-wrap').find('.select').removeClass('is-open');
-	});
-
-
-
-	//	#scroller
-	// ===============================================
-	$(".scroller-box").mCustomScrollbar({
-		scrollInertia:150,
-		advanced:{
-			updateOnContentResize: true
-		}
-	});
-
-
-
-	//	#tabs
-	// ===============================================
-	$('.tabs').tabslet({
-		attribute: 'href',
-		animation: true
-	});
-
-
-
-	//	#map
-	// ===============================================
-	if($(".map-box").length) {
-		ymaps.ready(function () {
-			var myMap = new ymaps.Map('map-box', {
-				center: [55.73773386, 37.72953624],
-				zoom: 17,
-				controls: []
-			});
-			myMap.behaviors.disable('scrollZoom');
-			var myPlacemark = new ymaps.Placemark(myMap.getCenter());
+		var block = $(this).closest(".js-search");
+		var box = block.find(".search-form");
+		box.find(".input").focus();
 		
-			myMap.geoObjects.add(myPlacemark);
+		if(!(box.hasClass("show"))) {
+			box.addClass("show");
+			thisis.fadeOut();
+			block.find(".close").show();
+		} else {
+			box.removeClass("show");
+			//block.find(".close").fadeIn();
+		}
+	});
+
+
+	//	#rating
+	// ===============================================
+	$('.rating__select').raty({
+		target     : '.rating__count',
+		targetType : 'score',
+		targetKeep : true,
+		starType   : 'div', 
+		number     : '10', 
+		score      : '1',
+	});
+
+
+	// #ripplelink
+	// ===============================================
+	$(function(){
+		var ink, d, x, y;
+		$(".ripplelink").click(function(e) {
+			if($(this).find(".ripplelink-ink").length === 0) {
+				$(this).prepend("<span class='ripplelink-ink'></span>");
+			}
+
+			ink = $(this).find(".ripplelink-ink");
+			ink.removeClass("ripplelink-animate");
+
+			if(!ink.height() && !ink.width()){
+				d = Math.max($(this).outerWidth(), $(this).outerHeight());
+				ink.css({height: d, width: d});
+			}
+
+			x = e.pageX - $(this).offset().left - ink.width()/2;
+			y = e.pageY - $(this).offset().top - ink.height()/2;
+
+			ink.css({top: y+'px', left: x+'px'}).addClass("ripplelink-animate");
 		});
-	}
+	});
+
+
 });
